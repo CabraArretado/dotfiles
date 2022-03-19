108047e89845a4c9b7d21ab9a0555edab1a31197
Felipe Moura <felipemouradev@gmail.com>
2022-03-19 15:00:05 -0500

    updates the create_symlink

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -6,21 +6,21 @@ read answer
 
 if [ $answer == "yes" ]; then
 
-PATH_VIMRC="$HOME/.vimrc"
-PATH_NVIMCONFIG="$HOME/.config/nvim/init.vim"
-PATH_TMUX="$HOME/.tmux.conf"
+    PATH_VIMRC="$HOME/.vimrc"
+    PATH_NVIMCONFIG="$HOME/.config/nvim/init.vim"
+    PATH_TMUX="$HOME/.tmux.conf"
 
-PATH_OLD="$HOME/.original_dotfiles"
-mkdir $PATH_OLD
+    PATH_OLD="$HOME/.original_dotfiles"
+    mkdir $PATH_OLD
 
-mv $PATH_VIMRC $PATH_OLD/.vimrc
-mv $PATH_NVIMCONFIG $PATH_OLD/init.vim
-mv $PATH_TMUX $PATH_OLD/.tmux.conf
+    mv $PATH_VIMRC $PATH_OLD/.vimrc
+    mv $PATH_NVIMCONFIG $PATH_OLD/init.vim
+    mv $PATH_TMUX $PATH_OLD/.tmux.conf
 
-DOTFILES_REPO="$HOME/dotfiles"
-ln -sf $PATH_VIMRC $DOTFILES_REPO/.vimrc
-ln -sf $PATH_NVIMCONFIG $DOTFILES_REPO/init.nvim
-ln -sf $PATH_TMUX $DOTFILES_REPO/.tmux.conf
+    DOTFILES_REPO="$HOME/dotfiles"
+    ln -sf $DOTFILES_REPO/.vimrc $PATH_VIMRC
+    ln -sf $DOTFILES_REPO/init.vim $PATH_NVIMCONFIG
+    ln -sf $DOTFILES_REPO/.tmux.conf $PATH_TMUX
 
-echo "symlinks created and old saved on ~/.oldfiles"
+    echo "symlinks created and old saved on ~/.oldfiles"
 fi
