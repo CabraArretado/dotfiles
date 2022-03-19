1f9836557fc52f6f7a8add7ee38843547fa9e08c
Felipe Moura <felipemouradev@gmail.com>
2022-03-19 13:51:16 -0500

    created the symlinks script

---
+++ create_symlinks.sh
@@ -0,0 +1,26 @@
+!#/bin/bash
+
+echo "Do you want create the symlinks?"
+echo "type yes"
+read answer
+
+if [ $answer == "yes" ]; then
+
+PATH_VIMRC="$HOME/.vimrc"
+PATH_NVIMCONFIG="$HOME/.config/nvim/init.vim"
+PATH_TMUX="$HOME/.tmux.conf"
+
+PATH_OLD="$HOME/.originaldotfiles"
+mkdir $PATH_OLD
+
+mv $PATH_VIMRC $PATH_OLD/.vimrc
+mv $PATH_NVIMCONFIG $PATH_OLD/init.vim
+mv $PATH_TMUX $PATH_OLD/.tmux.conf
+
+DOTFILES_REPO="$HOME/dotfiles"
+ln -sf $PATH_VIMRC $DOTFILES_REPO/.vimrc
+ln -sf $PATH_NVIMCONFIG $DOTFILES_REPO/init.nvim
+ln -sf $PATH_TMUX $DOTFILES_REPO/.tmux.conf
+
+echo "symlinks created and old saved on ~/.oldfiles"
+fi
