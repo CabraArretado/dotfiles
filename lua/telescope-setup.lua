5a144a28020e04f32981e3f1688cd116817dbf48
Felipe Moura <felipemouradev@gmail.com>
2022-09-03 00:53:09 -0500

    telescope new configs

--- lua/telescope-setup.lua
+++ lua/telescope-setup.lua
@@ -1,40 +1,48 @@
 -- local actions = require("telescope.actions")
 
-require('telescope').setup{
+require('telescope').setup {
   defaults = {
-    -- Default configuration for telescope goes here:
-    -- config_key = value,
-    mappings = {
-      i = {
-        -- map actions.which_key to <C-h> (default: <C-/>)
-        -- actions.which_key shows the mappings for your picker,
-        -- e.g. git_{create, delete, ...}_branch for the git_branches picker
-        -- ["<esc>"] = actions.close
+    vimgrep_arguments = {
+      "rg",
+      "--color=never",
+      "--no-heading",
+      "--with-filename",
+      "--line-number",
+      "--column",
+      "--smart-case",
+    },
+    prompt_prefix = " 🔍  ",
+    selection_caret = "  ",
+    entry_prefix = "  ",
+    selection_strategy = "reset",
+    sorting_strategy = "ascending",
+    layout_strategy = "horizontal",
+    layout_config = {
+      horizontal = {
+        prompt_position = "top",
       },
+      vertical = {
+        mirror = false,
+      },
+      preview_cutoff = 120,
+    },
+    file_ignore_patterns = { "node_modules" },
+    path_display = { "truncate" },
+    winblend = 0,
+    border = true,
+    color_devicons = true,
+    set_env = { ["COLORTERM"] = "truecolor" },
+    mappings = {
       n = {
+        ["q"] = require("telescope.actions").close,
         ["J"] = "preview_scrolling_down",
         ["K"] = "preview_scrolling_up",
-      }
-    }
-  },
-
-  pickers = {
-     oldfiles = {
-	only_cwd = true
       },
-    -- Default configuration for builtin pickers goes here:
-    -- picker_name = {
-    --   picker_config_key = value,
-    --   ...
-    -- }
-    -- Now the picker_config_key will be applied every time you call this
-    -- builtin picker
+    },
   },
-  extensions = {
-    -- Your extension configuration goes here:
-    -- extension_name = {
-    --   extension_config_key = value,
-    -- }
-    -- please take a look at the readme of the extension you want to configure
+  pickers = {
+    oldfiles = {
+      only_cwd = true
+    },
   }
 }
