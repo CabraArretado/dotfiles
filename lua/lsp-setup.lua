1c2d7df2b4cde717584838b3de8546ceb4810c08
Felipe Moura <felipemouradev@gmail.com>
2022-09-17 01:11:08 -0500

    Lua LSP

--- lua/lsp-setup.lua
+++ lua/lsp-setup.lua
@@ -39,40 +39,40 @@ local on_attach = function(client, bufnr)
 	vim.keymap.set('n', '<space>f', vim.lsp.buf.formatting, bufopts)
 end
 
--- lspconfig.sumneko_lua.setup {
--- 	capabilities = capabilities,
--- 	on_attach = on_attach,
--- 	flags = lsp_flags,
--- 	settings = {
--- 		Lua = {
--- 			runtime = {
--- 				-- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
--- 				version = 'LuaJIT',
--- 			},
--- 			diagnostics = {
--- 				-- Get the language server to recognize the `vim` global
--- 				globals = { 'vim' },
--- 			},
--- 			workspace = {
--- 				-- Make the server aware of Neovim runtime files
--- 				library = vim.api.nvim_get_runtime_file("", true),
--- 			},
--- 			-- Do not send telemetry data containing a randomized but unique identifier
--- 			telemetry = {
--- 				enable = false,
--- 			},
--- 			format = {
--- 				enable = true,
--- 				-- Put format options here
--- 				-- NOTE: the value should be STRING!!
--- 				defaultConfig = {
--- 					indent_style = "space",
--- 					indent_size = "2",
--- 				}
--- 			},
--- 		},
--- 	},
--- }
+lspconfig.sumneko_lua.setup {
+	capabilities = capabilities,
+	on_attach = on_attach,
+	flags = lsp_flags,
+	settings = {
+		Lua = {
+			runtime = {
+				-- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+				version = 'LuaJIT',
+			},
+			diagnostics = {
+				-- Get the language server to recognize the `vim` global
+				globals = { 'vim' },
+			},
+			workspace = {
+				-- Make the server aware of Neovim runtime files
+				library = vim.api.nvim_get_runtime_file("", true),
+			},
+			-- Do not send telemetry data containing a randomized but unique identifier
+			telemetry = {
+				enable = false,
+			},
+			format = {
+				enable = true,
+				-- Put format options here
+				-- NOTE: the value should be STRING!!
+				defaultConfig = {
+					indent_style = "space",
+					indent_size = "2",
+				}
+			},
+		},
+	},
+}
 -- List all servers here --
 -- tables of string with the server's names
 local servers = {'pyright', 'tsserver'}
