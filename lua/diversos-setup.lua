37b57eb4d2b0a52ee102b327d321982960e6fe53
Felipe Moura <felipemouradev@gmail.com>
2023-01-25 21:57:29 -0600

    new signs to gitsigns

--- lua/diversos-setup.lua
+++ lua/diversos-setup.lua
@@ -1,6 +1,6 @@
 -- Place to install small plugins setups
 
-local default_only_plugins = { 'hop', 'fidget', 'gitsigns' }
+local default_only_plugins = { 'hop', 'fidget' }
 
 -- Setup Plugins just with default setup
 for _, plugin in ipairs(default_only_plugins) do
@@ -11,3 +11,13 @@ require('indent_blankline').setup {
   char = '┊',
   show_trailing_blankline_indent = false,
 }
+
+require('gitsigns').setup({
+  signs = {
+    add = { text = '++' },
+    change = { text = '~~' },
+    delete = { text = '__' },
+    topdelete = { text = '‾‾' },
+    changedelete = { text = '~~' },
+  },
+})
